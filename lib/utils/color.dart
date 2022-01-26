33582463891793d2263348fe9c88b22b40d99f20
Abel Fernando Sangama Oñate <abelsangama@upeu.edu.pe>
2022-01-26 00:39:28 -0500

    subeindo colores

---
+++ lib/utils/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color homePageBackground = const Color(0xFFfbfcff);
+  static Color gradientFirst = const Color(0xFFfbfcff);
+  static Color gradientSecond = const Color(0xFFfbfcff);
+  static Color homePageTtle = const Color(0xFFfbfcff);
+}
